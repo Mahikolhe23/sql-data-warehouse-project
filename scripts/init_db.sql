fd32706f7ee67a4efeefc7f57fd0e23e492ffc8f
Mahendra Kolhe <mkolhe23@gmail.com>
2025-02-22 23:07:02 +0530

    sql project initialization

---
+++ scripts/init_db.sql
@@ -0,0 +1,37 @@
+/*
+
+==============================================================================
+Create Database and schemas
+==============================================================================
+Script purpose:
+    This script is use to create Datawarehouse DB and Schemas bronze, silver, gold
+
+*/
+
+USE master;
+GO
+
+-- Drop and recreate Datawarehouse DB
+IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'Datawarehouse')
+BEGIN
+    ALTER DATABASE Datawarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+    DROP DATABASE Datawarehouse;
+END;
+GO
+
+-- Create DB Datawarehouse
+CREATE DATABASE Datawarehouse;
+GO
+
+USE Datawarehouse;
+GO
+
+-- Create Schemas
+CREATE SCHEMA bronze;
+GO
+
+CREATE SCHEMA silver;
+GO
+
+CREATE SCHEMA gold;
+GO
